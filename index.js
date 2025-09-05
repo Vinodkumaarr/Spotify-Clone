c55debd0a3251f62d5bf3f83fed8017520d981fe
Vinodkumaarr <160580596+Vinodkumaarr@users.noreply.github.com>
2025-09-05 18:01:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" href="https://cdn-icons-png.flaticon.com/128/2585/2585161.png">
+    <title>Spotify-Web Player:Music for everyone</title>
+    <link rel="stylesheet" href="stylepro.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&family=Playfair+Display:wght@400;500;600;700&family=Poppins:ital,wght@1,300&display=swap" rel="stylesheet">
+    
+</head>
+<body>
+    <div class="main">
+        <div class="sidebar">
+         <div class="nav">
+          <div class="nav-option" style="opacity: 1;">
+            <i class="fa-solid fa-house"></i>
+            <a href="#">Home</a>
+            </div>
+            <div class="nav-option">
+             <i class="fa-solid fa-magnifying-glass"></i>
+             <a href="#">Search</a>
+            </div>
+        </div>
+            <div class="library">
+                <div class="options">
+                <div class="lib-option nav-option">
+                     <img src="./assets/library_icon.png">
+                    <a href="#">Your library</a>
+                </div>
+                <div class="icons">
+                    <i class="fa-solid fa-plus"></i>
+                    <i class="fa-solid fa-arrow-right"></i>
+                </div>
+                </div>
+                <div class="lib-box">
+                    <div class="box">
+                        <p class="box-p1">Creat your first playlist</p>
+                        <p class="box-p2">It's easy,we'll help you</p>
+                        <button class="badge">Creat playlist</button>
+                    </div>
+                    <div class="box">
+                        <p class="box-p1">Let's find some podcasts to follow</p>
+                        <p class="box-p2">We'll keep you updated on new episodes</p>
+                        <button class="badge">Brows podcasts</button>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <div class="main-content">
+            <div class="sticky-nav">
+                <div class="sticky-nav-icons">
+                <i class="fa-solid fa-code" class="hide"></i>
+            </div>
+                <div class="sticky-nav-options">
+                <button class="badge nav-item hide">explore Premium</button>
+                <button class="badge nav-item dark-badge"><i class="fa-solid fa-download" style="margin-right: 5px;"></i>Install App</button>
+                <i class="fa-solid fa-user nav-item"></i>
+            </div>
+                
+            </div>
+            
+                <h2>Recently Played</h2>
+                <div class="card-container">
+                    <div class="card">
+                       <img src="https://img.freepik.com/free-psd/music-festival-square-flyer-template_23-2149962690.jpg?w=740&t=st=1696105120~exp=1696105720~hmac=4deeb93d7a156b1949ca8e9dd5fc2ab67ee90d40c5f8694dc3e6355fc5ad213f" class="card-img">
+                       <p class="card-title">Top-50-Global</p>
+                       <p class="card-info">Your daily update of most played tracks....</p>
+                   
+                </div>
+            </div>
+            <h2>Trending now near you</h2>
+                <div class="cards-container">
+                    <div class="card">
+                       <img src="https://img.freepik.com/free-vector/music-player-application-interface-template-collection_23-2148530265.jpg?w=740&t=st=1696106557~exp=1696107157~hmac=78931383017adfd2e1f5074a12e9960679d729e56667568ef53198fef29477a4" class="card-img">
+                       <p class="card-title">Top-50-Global</p>
+                       <p class="card-info">Your daily update of most played tracks....</p>
+                   </div>
+                       <div class="card">
+                       <img src="https://img.freepik.com/free-photo/music-sound-multimedia-player-concept_53876-122591.jpg?w=900&t=st=1696106793~exp=1696107393~hmac=78f0ec3a733a247c4dfba657a530a61504a412f101b15113497b5c422548588f" class="card-img">
+                       <p class="card-title">Top-50-Global</p>
+                       <p class="card-info">Your daily update of most played tracks....</p>
+                   </div>
+            
+                    <div class="card">
+                       <img src="https://img.freepik.com/free-photo/phone-with-music-icon-headphones-blurred-background-music-listening-concept-copy-space_169016-14360.jpg?w=900&t=st=1696106925~exp=1696107525~hmac=a94429ed451e44382054a23371fd6f63f7bb82ba54d728f8d2fea8413c6fd5c2" class="card-img">
+                       <p class="card-title">Top-50-Global</p>
+                       <p class="card-info">Your daily update of most played tracks....</p>
+                   </div>
+                    <div class="card">
+                       <img src="https://img.freepik.com/free-photo/online-music-store_53876-167068.jpg?w=900&t=st=1696107182~exp=1696107782~hmac=945f63d256811f26deba8835c391666ca0409c776ddf93922c90cac95657dd86" class="card-img">
+                       <p class="card-title">Top-50-Global</p>
+                       <p class="card-info">Your daily update of most played tracks....</p>
+                   
+                </div>
+                <div class="card">
+                    <img src="https://lh3.googleusercontent.com/zkEH0cjqjSqIn6Y0Hx4eaFPBb0kgYlZMG6Y9iwdSbzLDgGrHMh8m_j4wvIGnwnof9OuOcszQVC4LfJEk=w544-h544-l90-rj" class="card-img">
+                    <p class="card-title">Top-50-Global</p>
+                    <p class="card-info">Your daily update of most played tracks....</p>
+                
+             </div>
+            </div>
+            <h2>Amazing Playlists</h2>
+                <div class="cards-container">
+                    <div class="card">
+                       <img src="https://img.freepik.com/free-psd/radio-music-streaming-smartphone_53876-355.jpg?size=626&ext=jpg&ga=GA1.1.2061778596.1696104164&semt=sph" class="card-img">
+                       <p class="card-title">Top-50-Global</p>
+                       <p class="card-info">Your daily update of most played tracks....</p>
+                   </div>
+            
+                  <div class="card">
+                       <img src="https://lh3.googleusercontent.com/zkEH0cjqjSqIn6Y0Hx4eaFPBb0kgYlZMG6Y9iwdSbzLDgGrHMh8m_j4wvIGnwnof9OuOcszQVC4LfJEk=w544-h544-l90-rj" class="card-img">
+                       <p class="card-title">Top-50-Global</p>
+                       <p class="card-info">Your daily update of most played tracks....</p>
+                   </div>
+                  <div class="card">
+                       <img src="https://music.youtube.com/image/mixart?r=ENgEGNgEMjoIARoLL20vMDEyZDZiMnAaCi9tLzAyNl85YzIaDS9nLzExcnhjN2drcm0aCi9tLzAxcHJfajYiAmVu" class="card-img">
+                       <p class="card-title">Top-50-Global</p>
+                       <p class="card-info">Your daily update of most played tracks....</p>
+                   </div>
+            
+                    <div class="card">
+                       <img src="https://music.youtube.com/image/mixart?r=ENgEGNgEMh8ICBABGgovbS8waGNjM3pmGgkvbS8wOGhyNzIiAmVu" class="card-img">
+                       <p class="card-title">Top-50-Global</p>
+                       <p class="card-info">Your daily update of most played tracks....</p>
+                   
+                </div>
+            </div>
+            <h2>Featured Charts</h2>
+                <div class="cards-container">
+                    <div class="card">
+                       <img src="https://yt3.ggpht.com/4qRV3ufC8BliwLhJ_Nc35ez5gK73m7kmyUa_-8u908sAA3IH5bmQ3voUVdnPMwwXKQhrd33ockU=s576" class="card-img">
+                       <p class="card-title">Top-50-Global</p>
+                       <p class="card-info">Your daily update of most played tracks....</p>
+                   </div>
+            
+                  <div class="card">
+                       <img src="https://yt3.ggpht.com/Xbh97yydekQTzF8OpDD_HynM7vBC6NjvPGuhY2Ci1mHaQhRG6e7AizBkz3ErtewmeVU4AKPYtg4B=s576" class="card-img">
+                       <p class="card-title">Top-50-Global</p>
+                       <p class="card-info">Your daily update of most played tracks....</p>
+                   </div>
+                  <div class="card">
+                       <img src="https://lh3.googleusercontent.com/Oz7OsKk9cZNQzkISSM3vmsxzXOI3_vAByMetk9dqC03moH-WXimzNZimppEM3hEyjPbknviSvBseC6A=w544-h544-l90-rj" class="card-img">
+                       <p class="card-title">Top-50-Global</p>
+                       <p class="card-info">Your daily update of most played tracks....</p>
+                   </div>
+            
+                    <div class="card">
+                       <img src="https://lh3.googleusercontent.com/qJ4zLa_7RkHPFrz7wsmiwz67YGDejvttuyxct4s4ziczP8eSKgLk1N3OEgJ7K0jc_kDJCsWg7heOvQ=w544-h544-l90-rj" class="card-img">
+                       <p class="card-title">Top-50-Global</p>
+                       <p class="card-info">Your daily update of most played tracks....</p>
+                   
+                </div>
+            </div>
+            <div class="footer">
+                <div class="line"></div>
+            </div>
+        </div>
+        <!--<div class="music-player">
+            <div class="album"></div>
+            <div  class="player">
+             <div class="player-control">
+                <img src="https://cdn-icons-png.flaticon.com/128/8839/8839055.png" class="player-music-icon1">
+                <img src="https://cdn-icons-png.flaticon.com/128/196/196092.png" class="player-music-icon">
+                <img src="https://cdn-icons-png.flaticon.com/128/483/483025.png" class="player-music-icon">
+                <img src="">
+            </div>
+            <div class="progress-bar">
+                <span class="current-time">00:00</span>
+                <input type="range" min="0" max="100"  class="progress-bar">
+                <span class="current-time">00:00</span>
+            </div>-->
+        </div>
+        <div  class="controls"></div>
+    </div>
+    
+</body>
+</html>
+
